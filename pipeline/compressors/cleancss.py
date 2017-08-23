be40b2d6a0845e19c5870caac38a4f210c625cb1
Frankie Dintino <fdintino@gmail.com>
2017-08-23 10:28:51 -0400

    Handle source map file paths with STATICFILES_DIRS, fail gracefully
    If a sourcemap source fails to resolve with the static file finder, log
    the error but don't raise an exception during collectstatic.

--- pipeline/compressors/cleancss.py
+++ pipeline/compressors/cleancss.py
@@ -3,12 +3,18 @@ from __future__ import unicode_literals
 import codecs
 import json
 import os
+import logging
 
+from django.conf import settings as django_settings
 from django.contrib.staticfiles.storage import staticfiles_storage
+from django.core.exceptions import SuspiciousFileOperation
 
 from pipeline.conf import settings
 from pipeline.compressors import SubProcessCompressor
 from pipeline.utils import source_map_re, relurl
+
+
+logger = logging.getLogger('pipeline')
 
 
 class CleanCSSCompressor(SubProcessCompressor):
@@ -56,10 +62,25 @@ class CleanCSSCompressor(SubProcessCompressor):
         # Grab urls from staticfiles storage (in case filenames are hashed)
         source_map_data = json.loads(source_map)
         for i, source in enumerate(source_map_data['sources']):
-            source_abs_path = os.path.join(output_dir, source)
+            source_abs_path = os.path.abspath(os.path.join(output_dir, source))
             source_rel_path = os.path.relpath(
                 source_abs_path, staticfiles_storage.base_location)
-            source_url = staticfiles_storage.url(source_rel_path)
+            source_url = None
+            try:
+                source_url = staticfiles_storage.url(source_rel_path)
+            except SuspiciousFileOperation:
+                for static_dir in django_settings.STATICFILES_DIRS:
+                    if not isinstance(static_dir, tuple):
+                        continue
+                    prefix, path = static_dir
+                    if not source_abs_path.startswith(path):
+                        continue
+                    source_rel_path = os.path.relpath(source_abs_path, path)
+                    source_url = staticfiles_storage.url(
+                        os.path.join(prefix, source_rel_path))
+                if not source_url:
+                    logger.exception("Could not relativize source map file")
+                    continue
             source_map_data['sources'][i] = relurl(source_url, output_url)
         source_map = json.dumps(source_map_data)
 
